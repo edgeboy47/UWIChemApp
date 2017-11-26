b4ab3cabbd59ccc5a9c87687cf6c30561605268d
GidSchwifty <gidramch@gmail.com>
2017-11-26 12:43:37 -0400

    FirebaseEventIntegration

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, ModalController, AlertController, NavParams } from 'ionic-angular';
 import * as moment from 'moment';
+import {AngularFireDatabase} from 'angularfire2/database';
 
 /**
  * Generated class for the CalendarPage page.
@@ -18,84 +19,115 @@ export class CalendarPage {
 
   courseID;
   
-    calendar = {
-      mode: 'month',
-      currentDate: new Date()
-    };
-  
-    viewTitle: string;
-    eventSource = [];
-    selectedDay = new Date();
-  
-    constructor(public navCtrl: NavController,  private modalCtrl:ModalController, private alertCtrl: AlertController, public navParams: NavParams) {
-  
-    }
-  
-    ionViewDidLoad() {
-      this.courseID = this.navParams.get('courseID');
-      console.log(this.courseID);
-    }
-  
-    addEvent(){
-      let modal = this.modalCtrl.create('EventModalPage',{selectedDay:this.selectedDay});
-      modal.present();
-      modal.onDidDismiss(data=>{
-        if(data){
-          let eventData = data;
-  
-          eventData.startTime = new Date(data.startTime);
-          eventData.endTime = new Date(data.endTime);
-  
-          let events = this.eventSource;
-          events.push(eventData);
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  };
 
-          this.eventSource = [];
-          setTimeout(()=>{
-            this.eventSource = events;
-          });
-        }
-      })
-    }
-  
-    onViewTitleChanged(title){
-      this.viewTitle = title;
-    }
-  
-    onTimeSelected(ev){
-      this.selectedDay = ev.selectedTime;
-    }
-  
-    removeEvent(event){
-      let events = this.eventSource;
-      events.splice(events.indexOf(event),1);
-      
+  viewTitle: string;
+  eventSource = [];
+  selectedDay = new Date();
+
+  constructor(public navCtrl: NavController,  private modalCtrl:ModalController, private alertCtrl: AlertController, public navParams: NavParams, public db: AngularFireDatabase) {
+
+  }
+
+  ionViewDidLoad() {
+    this.courseID = this.navParams.get('courseID');
+    console.log(this.courseID);
+    this.db.object('/Events/'+this.courseID).valueChanges().subscribe(data=>{
+      let events= this.eventSource;
+      for(let key in data){
+        let d = data[key];
+        let ev = {startTime: new Date(), endTime: new Date(), title: "", type: "", id:""};
+        
+        ev.endTime = new Date(d['date']);
+        ev.startTime = ev.endTime;
+        ev.title = d['Notes'];
+        ev.type = d['Type'];
+        
+        ev.id = key;
+
+        events.push(ev);
+      }
+
       this.eventSource = [];
       setTimeout(()=>{
         this.eventSource = events;
       });
-      
-    }
+    });
+  }
 
-    onEventSelected(event){
-      let end = moment(event.endTime).format('LLLL');
-  
-      let alert = this.alertCtrl.create({
-        title: ''+event.title,
-        subTitle: 'Due Date: '+end,
-        message: 'Type: '+event.type,
-        buttons: [
-          {
-            text: 'OK',
-          },
-          {
-            text: 'Remove',
-            handler: ()=>{
-              this.removeEvent(event);
-            }
+  addEvent(){
+    let modal = this.modalCtrl.create('EventModalPage',{selectedDay:this.selectedDay});
+    modal.present();
+    modal.onDidDismiss(data=>{
+      if(data){
+        let eventData = data;
+
+        eventData.startTime = new Date(data.startTime);
+        eventData.endTime = new Date(data.endTime);
+
+        this.db.list('/Events/'+this.courseID).push({
+          date: eventData.endTime.toISOString(),
+          Notes: eventData.title,
+          Type: eventData.type,
+        });
+
+        let events = this.eventSource;
+        events.push(eventData);
+
+        this.eventSource = [];
+        setTimeout(()=>{
+          this.eventSource = events;
+        });
+      }
+    })
+  }
+
+  onViewTitleChanged(title){
+    this.viewTitle = title;
+  }
+
+  onTimeSelected(ev){
+    this.selectedDay = ev.selectedTime;
+  }
+
+  removeEvent(event){
+    let events = this.eventSource;
+    events.splice(events.indexOf(event),1);
+    
+    this.db.object('/Events/'+this.courseID+'/'+event.id+'/').remove();
+
+    this.eventSource = [];
+    setTimeout(()=>{
+      this.eventSource = events;
+    });
+    
+  }
+
+  onEventSelected(event){
+    console.log(event.endTime);
+    let end = moment(event.endTime).format('LLLL');
+    console.log(end);
+
+    let alert = this.alertCtrl.create({
+      title: ''+event.title,
+      subTitle: 'Due Date: '+end,
+      message: 'Type: '+event.type,
+      buttons: [
+        {
+          text: 'OK',
+        },
+        {
+          text: 'Remove',
+          handler: ()=>{
+            this.removeEvent(event);
           }
-        ]
-      });
-      alert.present();
-    }
+        }
+      ]
+    });
+    alert.present();
+  }
 
 }
