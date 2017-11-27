ad2d1f1c26bf2b6b97c2e975e48599565f11e90d
KrystelManning <krystelmanning@hotmail.com>
2017-11-27 00:13:55 -0400

    notices page updated, almost finished

--- src/pages/notices/notices.ts
+++ src/pages/notices/notices.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+//import {AngularFireDatabase} from 'angularfire2/database';
+//import { AngularFireAuth } from 'angularfire2/auth';
 /**
  * Generated class for the NoticesPage page.
  *
@@ -14,12 +15,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'notices.html',
 })
 export class NoticesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  notices:any = [];
+  constructor( public navCtrl: NavController, public navParams: NavParams, public alertCtrl:AlertController) {
   }
-
+//private fbAuth: AngularFireAuth,
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NoticesPage');
+    console.log('ionViewDidLoad UserNoticesPage');
+    /*this.fbAuth.authState.subscribe(data=>{
+      this.db.object('/UserNotices/'+data.uid).valueChanges().subscribe(data=>{
+        this.notices= [];
+        for(let key in data){
+          let notice = {noticeID:key,Message:data[key]};
+          this.notices.push(notice);
+        }
+      });
+    }); */
   }
 
+  navigateToCalendar(noticeID:string){
+    //this.navCtrl.push('CalendarPage',{noticeID});
+  }
+
+  removeNotice(noticeID:string){
+    /*this.fbAuth.authState.subscribe(data=>{
+      this.db.object('/UserNotices/'+data.uid+'/'+noticeID+'/').remove()
+    });*/
+  }
+
+
+  addNotice(){
+     this.alertCtrl.create({
+      title:"Add a notice",
+      inputs:[
+        {
+          name:'To',
+          placeholder:'Recipient'
+        },
+        {
+          name:'Message',
+          placeholder:'Message'
+        },
+        {
+          name:'Date',
+          placeholder:'Date'
+        }
+      ],
+      buttons:[
+        {
+          text:'Submit',
+          role:'submit',
+          handler:data=>{
+
+          }
+        },
+        {
+          text:'Cancel',
+          role:'cancel',
+          handler:data=>{
+              console.log("Cancel button clicked..")
+          }
+        }
+
+      ]
+     }).present()
+  }
 }
