035033c4183f2c07fdf0e89bc8d57fe648782ec9
edgeboy47 <dmills1994@gmail.com>
2017-12-01 16:09:38 -0400

    Sub/unsub user from course topic when they add/del respective course

--- functions/index.js
+++ functions/index.js
@@ -27,15 +27,35 @@ exports.notification = functions.database.ref("Notices/{noticeHead}").onCreate( 
     })
 })
 
-// Subscribes users to a course topic when they add one to their course list
-// exports.subscribeToCourse = functions.database.ref("UserCourses/{userID}/{courseName}").onWrite( event => {
-//     const oldVal = event.data.previous.val()
-//     const newVal = event.data.current.val()
 
-//     if(newVal != null){
+//Subscribes users to a course topic when they add one to their course list, and unsubscribes them when they delete a course
+exports.subscribeToCourse = functions.database.ref("UserCourses/{userID}/{courseCode}").onWrite( event => {
+    // Gives the course name before the event was triggered
+    const oldVal = event.data.previous.val()
 
-//     }
-// })
+    // Gives the course name after the event was triggered
+    const newVal = event.data.current.val()
+
+    const topic = String(event.params.courseCode).split(' ').join('')
+    
+    // If a new course is added
+    if(oldVal === null && newVal != null) {
+        admin.database().ref(`Users/${event.params.userID}/notificationToken`).on("value", currentVal => {
+            admin.messaging().subscribeToTopic(currentVal.val(), topic).then( res => {
+                console.log(`User: ${event.params.userID} successfully subscribed to topic: ${topic}`)
+            })
+        })
+    }
+
+    // If a course is deleted
+    if(oldVal != null && newVal === null) {
+        admin.database().ref(`Users/${event.params.userID}/notificationToken`).on("value", currentVal => {
+            admin.messaging().unsubscribeFromTopic(currentVal.val(), topic).then( res => {
+                console.log(`User: ${event.params.userID} successfully unsubscribed from topic: ${topic}`)
+            })
+        })
+    }
+})
 
 // Subscribes new users to the global topic
 exports.globalNotification = functions.database.ref("Users/{userID}").onUpdate( event => {
