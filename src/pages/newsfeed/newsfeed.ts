1bcbea44eb3c8995bd00086d8b3c8bda1479a43a
GidSchwifty <gidramch@gmail.com>
2018-02-19 19:54:29 -0400

    added mock newsfeed item

--- src/pages/newsfeed/newsfeed.ts
+++ src/pages/newsfeed/newsfeed.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';        //Import AngularFireAuth Modular for authentication.
 
 /**
  * Generated class for the NewsfeedPage page.
@@ -15,10 +17,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewsfeedPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  showButtons = false;                              //Boolean indicating whether certain buttons should be shown based on user type.
+  userSub;
+  typeSub;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public db: AngularFireDatabase,
+              public auth: AngularFireAuth        //Various constructor declarations
+            ) {
   }
 
   ionViewDidLoad() {
+    this.userSub = this.auth.authState.subscribe(data=>{
+      if(data){
+        this.typeSub = this.db.object('/Users/'+data.uid+'/type/').valueChanges().subscribe(d2=>{
+          if(d2=='Admin'){
+            this.showButtons = true;              //Get the user type and set the showButtons variable according to the type of user. Admin users are allowed to add courses.
+          }
+        });
+      }
+    });
   }
 
+  addNews(){
+    
+  }
 }
